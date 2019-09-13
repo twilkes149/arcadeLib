80fe0e6ec19ad732d5977f6d8ad0d85bb0a82a10
Tucker <twilkes149@gmail.com>
2019-09-12 22:15:37 -0600

    added documentation for animation sequences

--- lib/sprite.h
+++ lib/sprite.h
@@ -1,15 +1,26 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <dirent.h>
+#include <stdio.h>
 #include "frame.h"
 
+#define SPRITE_MAX_FRAMES 18
+#define SPRITE_MAX_FRAME_NAME_LENGTH 18
+
 struct sprite {
-	uint16_t row, col;
+	//Positioning variables
+	uint16_t row, col;//the row and col of the top left corner of where to draw the sprite
 	uint16_t height, width;
-	uint16_t scale;
+	uint16_t scale;//howw big to draw the sprite compared to what is in the sprite file
 	
+	uint8_t currentFrame;//the current frame that is being displayed
+	uint8_t numFrames;//how many frames are in the sprite's animation sequence
 	struct frame* frames;
 };
+
+//function to init the sprite with all of the frames in the provided file
+void sprite_init(char* directory, struct sprite* _sprite);
 
 //functions to get information about a sprite
 uint16_t sprite_getRow();
@@ -22,4 +33,9 @@ uint16_t sprite_getHeight();
 
 uint16_t sprite_getScale();
 
-struct frame* sprite_getFrame(uint8_t frameIndex);
+struct frame* sprite_getFrame(uint8_t frameIndex);//returns the frame object at a specific index
+
+//function to draw the next frame in the sprite's sequence
+void sprite_animate(struct sprite* _sprite);
+
+void sprite_destroy();//function to destroy a sprite
--- testLib.c
+++ testLib.c
@@ -1,22 +1,42 @@
 #include "./lib/video.h"
 #include "./lib/color.h"
 #include "./lib/frame.h"
+#include "./lib/sprite.h"
 #include <stdlib.h>
 #include <stdint.h>
+#include <signal.h>
 
 int main(void) {	
 	uint32_t errorCode = 32;	
 	color_init("test.color", errorCode);
-	
-	struct frame circle;
-	circle.scale = 5;
-	frame_init("circle.frame", &circle);//read in a cirlce frame
+	/*
+	struct frame pacman0, pacman1;
+	pacman0.scale = 5;
+	pacman1.scale = 5;
+	frame_init("./Pacman/pacman0.frame", &pacman0);//read in a cirlce frame
+	frame_init("./Pacman/pacman1.frame", &pacman1);//read in a cirlce frame
 	
 	//init video and draw a line of color t
-	video_init();		
 	
-	video_drawFrame(100, 100, &circle);	
-	frame_destroy(&circle);
+	
+	while (1) {
+		video_drawFrame(100, 100, &pacman0);
+		for (uint64_t i = 0; i < 20000000; i++);
+		video_drawFrame(100, 100, &pacman1);
+		for (uint64_t i = 0; i < 20000000; i++);
+	}
+	
+	
+	
+	frame_destroy(&pacman0);
+	frame_destroy(&pacman1);
+	*/
+	video_init();
+	
+	struct sprite pacman;
+	sprite_init("./Pacman", &pacman);
+	
 	video_close();
+	
 	return 0;
 }
